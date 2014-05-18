3ac43ba36ee8ae85551b076af176a59aebe46c85
Zac Mrowicki <zmrowicki@hotmail.com>
2014-05-18 12:18:51 -0700

    Basic hover functinality done

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	for(var i = 1; i < 257; i++ ) {
 		$('#grid').append('<div class="square"></div>');
 	};
+	
+	$(".square").hover(function() {
+		$(this).css('background-color', "black");
+		$(this).css('border', "1px solid white");
+	});
 })
