7d88cb9e0c79eef273535e4820b609ab98ef256e
Zac Mrowicki <zmrowicki@hotmail.com>
2014-05-19 21:00:43 -0700

    Add prompt for size of grid

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+
 $(document).ready(function() {
-	for(var i = 1; i < 257; i++ ) {
-		$('#grid').append('<div class="square"></div>');
-	};
+	var $size = prompt("How large should one side of the grid be? (1-128)");
+	
+	if($size >= 1 && $size <= 128 ){ //Make sure the size is within parameters
+		var gridSquareSize = $('#grid').width() / $size-2;
+		console.log(gridSquareSize)
+
+		for(var i = 1; i < ($size * $size); i++ ) {
+			$('#grid').append('<div class="square"></div>');
+		};
+		$(".square").css('width', gridSquareSize);
+		$(".square").css('height', gridSquareSize);
+		$(".square").css('display', 'inline-block')
+	}
 	
 	$(".square").hover(function() {
 		$(this).css('background-color', "black");
@@ -10,5 +21,6 @@ $(document).ready(function() {
 
 	$("button").click(function() {
 		$('.square').removeAttr('style');
+		$size;
 	});
 })
