ef821df2ef6d58d8c403f34f79d7e0f113cbbe16
Zac Mrowicki <zmrowicki@hotmail.com>
2014-05-19 22:05:39 -0700

    Split out into functions, now nothing is working

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
+// Script for the Etch-a-Scetch!!
 
 $(document).ready(function() {
-	var $size = prompt("How large should one side of the grid be? (1-128)");
+	alert("Click on a button to choose what type of Etch-a-Scetch you want!")
 	
-	if($size >= 1 && $size <= 128 ){ //Make sure the size is within parameters
-		var gridSquareSize = $('#grid').width() / $size-2;
-		console.log(gridSquareSize)
-
-		for(var i = 1; i < ($size * $size); i++ ) {
-			$('#grid').append('<div class="square"></div>');
-		};
-		$(".square").css('width', gridSquareSize);
-		$(".square").css('height', gridSquareSize);
-		$(".square").css('display', 'inline-block')
+	function black(){
+		$("#black").click(function() {
+			initializeGrid();
+			$(".square").hover(function() {
+				$(this).css('background-color', "black");
+				$(this).css('border', "1px solid white");
+			});
+		});
 	}
-	
-	$(".square").hover(function() {
-		$(this).css('background-color', "black");
-		$(this).css('border', "1px solid white");
-	});
 
-	$("button").click(function() {
-		$('.square').removeAttr('style');
-		$size;
-	});
+	function trail() {
+		$("#trail").click(function () {
+			initializeGrid();
+			$(".square").hover(function() {
+				$(this).fadeIn(300);
+				$(this).fadeOut(300);
+			});
+		});
+	}
+
+	function reset() {
+		$("#clear").click(function() {
+			initializeGrid();
+			$('.square').removeAttr('style');
+		});
+		alert("Please play again!  Click on an Etch-a-Sketch type again!")
+	}
+
+	function initializeGrid() {
+		var $size = prompt("How large should one side of the grid be? (1-128)");
+
+		if($size >= 1 && $size <= 128 ){ //Make sure the size is within parameters
+			var gridSquareSize = $('#grid').width() / $size-2;
+			console.log(gridSquareSize)
+
+				for(var i = 1; i < ($size * $size); i++ ) {
+					$('#grid').append('<div class="square"></div>');
+				};
+			$(".square").css('width', gridSquareSize);
+			$(".square").css('height', gridSquareSize);
+			$(".square").css('display', 'inline-block')
+		} else {
+			alert("Please try again");
+		};
+	};
 })
